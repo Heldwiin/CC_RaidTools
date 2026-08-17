6fb9f2cfcca77970a9b878399cd93d41a5756c2c
Heldwiin <84325986+Heldwiin@users.noreply.github.com>
2026-08-17 22:45:28 +0200

    Match world marker icons to raid marker order

--- MarksBar.lua
+++ MarksBar.lua
@@ -15,7 +15,9 @@ if db.x == nil then db.x = 0 end
 if db.y == nil then db.y = -180 end
 
 local MARK_ICON_TEXTURE = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_"
-local WORLD_MARK_ICON = { [1]=6, [2]=4, [3]=3, [4]=7, [5]=1, [6]=2, [7]=5, [8]=8 }
+-- World marker commands keep their IDs, but the displayed icons follow the same visual order as the raid markers.
+-- Visual order: star, circle, diamond, triangle, moon, square, cross, skull.
+local WORLD_MARK_COMMAND = { [1]=5, [2]=6, [3]=3, [4]=2, [5]=7, [6]=1, [7]=4, [8]=8 }
 local bar
 local buttons = {}
 local worldButtons = {}
@@ -35,12 +37,6 @@ end
 
 local function SetMarkIcon(texture, index)
     texture:SetTexture(MARK_ICON_TEXTURE .. index)
-    texture:SetTexCoord(0, 1, 0, 1)
-end
-
-local function SetWorldMarkIcon(texture, markerID)
-    local iconID = WORLD_MARK_ICON[markerID] or markerID
-    texture:SetTexture(MARK_ICON_TEXTURE .. iconID)
     texture:SetTexCoord(0, 1, 0, 1)
 end
 
@@ -147,8 +143,11 @@ local function CreateBar()
         AddTooltip(b, "Marque de raid " .. i, "Clic gauche : poser   |   Clic droit : retirer"); buttons[i] = b
     end
     for i = 1, 8 do
-        local b = MakeIconButton(bar, 23, 23); SetWorldMarkIcon(b.icon, i); b.icon:SetVertexColor(1, 0.85, 0.35, 0.85); SetupSecureWorldButton(b, i)
-        AddTooltip(b, "Marqueur au sol " .. i, "Clic gauche : placer   |   Clic droit : retirer"); worldButtons[i] = b
+        local b = MakeIconButton(bar, 23, 23)
+        SetMarkIcon(b.icon, i)
+        b.icon:SetVertexColor(1, 0.85, 0.35, 0.85)
+        SetupSecureWorldButton(b, WORLD_MARK_COMMAND[i])
+        AddTooltip(b, "Marqueur au sol " .. WORLD_MARK_COMMAND[i], "Clic gauche : placer   |   Clic droit : retirer"); worldButtons[i] = b
     end
     RestorePosition(); ApplyBarLayout()
     if not InCombatLockdown() then bar:SetShown(db.enabled) end
