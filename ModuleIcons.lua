a1294ff51dcb92fb4099b3f7ac6d3950f3a3f978
Heldwiin <84325986+Heldwiin@users.noreply.github.com>
2026-08-18 15:28:15 +0200

    Add WoW icons to module menu

---
+++ ModuleIcons.lua
@@ -0,0 +1,48 @@
+-- CC RaidTools - Module menu icons
+-- Adds small native WoW icons to the module selector.
+local ADDON_NAME = "CC_RaidTools"
+
+local MODULE_ICONS = {
+    AutoPromote = "Interface\\Icons\\Achievement_GuildPerk_MassResurrection",
+    AutoLog     = "Interface\\Icons\\INV_Misc_Book_09",
+    ReadyCheck  = "Interface\\Icons\\Spell_Holy_DivineProtection",
+    InviteTool  = "Interface\\Icons\\INV_Misc_GroupLooking",
+    Focus       = "Interface\\Icons\\Ability_Hunter_MarkedForDeath",
+    MarksBar    = "Interface\\Icons\\Achievement_Raid_DragonSoul_MadnessofDeathwing",
+}
+
+local function ApplyModuleIcons()
+    if not CCRT or not CCRT.GetMainFrame then return end
+    local frame = CCRT.GetMainFrame()
+    if not frame or not frame.menuButtons then return end
+
+    for moduleName, button in pairs(frame.menuButtons) do
+        local texturePath = MODULE_ICONS[moduleName]
+        if texturePath and not button._ccrtModuleIcon then
+            local icon = button:CreateTexture(nil, "ARTWORK")
+            icon:SetSize(20, 20)
+            icon:SetPoint("LEFT", button, "LEFT", 5, 0)
+            icon:SetTexture(texturePath)
+            icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+            button._ccrtModuleIcon = icon
+
+            if button.text then
+                button.text:ClearAllPoints()
+                button.text:SetPoint("LEFT", icon, "RIGHT", 6, 0)
+            end
+        end
+    end
+end
+
+local events = CreateFrame("Frame")
+events:RegisterEvent("ADDON_LOADED")
+events:SetScript("OnEvent", function(_, _, addonName)
+    if addonName ~= ADDON_NAME then return end
+    if CCRT and CCRT.ToggleUI then
+        local originalToggle = CCRT.ToggleUI
+        CCRT.ToggleUI = function(...)
+            originalToggle(...)
+            ApplyModuleIcons()
+        end
+    end
+end)
