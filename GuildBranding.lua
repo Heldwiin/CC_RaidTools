253f552936863e6f04ae11700eba6ff73f74a239
Heldwiin <84325986+Heldwiin@users.noreply.github.com>
2026-08-18 21:15:04 +0200

    Set guild watermark opacity to 95%

--- GuildBranding.lua
+++ GuildBranding.lua
@@ -13,7 +13,7 @@ local function ApplyGuildBranding()
     watermark:SetSize(170, 170)
     watermark:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -18, 18)
     watermark:SetTexture(LOGO)
-    watermark:SetAlpha(1.0)
+    watermark:SetAlpha(0.95)
     frame._ccrtWatermark = watermark
 end
 
